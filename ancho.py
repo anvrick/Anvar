cb7bdb6fef65746b7aeaec585cbf0747d47a9880
anvrick <pro100anco@gmail.com>
2024-12-17 14:29:55 +0500

    Update ancho.py
    Version 1.1

--- ancho.py
+++ ancho.py
@@ -12,3 +12,15 @@ class Warrior():
         print('Armor: ', self.armor)
         print('Weapon: ', self.weapon)
         print('Power: ', self.power, '\n')
+    
+    def fight(self, opponent):
+        print(self.name, 'encountered', opponent.name)
+        print(self.name, 'VS', opponent.name)
+        print('FIGHT')
+        print(self.name, 'attacks', opponent.name, 'with', self.weapon)
+        print(opponent.name, 'took damage.')
+        qoldi = opponent.armor - self.power
+        if qoldi >= 0:
+            print('His armor: ', qoldi, 'and His health: ', opponent.health)
+        elif qoldi <=0:
+            print(opponent.health - self.power, '\n')
